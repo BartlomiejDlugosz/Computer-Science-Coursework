a78f503401958f7d3d3129459c3e90c75b4b8de6
Bartlomiej Dlugosz <94141158+BartyCoding@users.noreply.github.com>
2022-11-07 11:56:36 +0000

    Added product and category post routes

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ const PORT = 3000
 
 app.use("view engine", "ejs")
 app.use("views", path.join(__dirname, "views"))
-
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 
 app.get("/", async (req, res) => {
@@ -30,6 +31,35 @@ app.get("/", async (req, res) => {
     res.render("homePage", {products})
 })
 
+
+
+
+app.get("/products", async (req, res) => {
+    const products = await Product.find({})
+    res.render("products", {products})
+})
+
+app.post("/products", async (req, res) => {
+    const {name, price, description, images, discount, discountedPrice, categories, tags} = req.body
+    const newProduct = new Product({name, price, description, images, discount, discountedPrice, categories, tags})
+    await newProduct.save()
+    res.redirect("/products")
+})
+
+
+
+
+app.get("/categories", async (req, res) => {
+    const categories = await Category.find({})
+    res.render("categories", {categories})
+})
+
+app.post("/categories", async (req, res) => {
+    const {name, description} = req.body
+    const newCategory = new Category({name, description})
+    await newCategory.save()
+    res.redirect("/categories")
+})
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}!`)
