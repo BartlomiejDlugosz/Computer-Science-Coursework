f8a07a402506e3a0e1e719d7511871fa59d0b5bd
Bartlomiej Dlugosz <94141158+BartyCoding@users.noreply.github.com>
2022-11-10 15:41:18 +0000

    Added Cart viewer

--- public/js/cartLoader.js
+++ public/js/cartLoader.js
@@ -1,7 +1,36 @@
 let cart = localStorage.getItem("cart")
+const template = document.querySelector("#template")
+
+async function addItem(productId) {
+    fetch(`/getProductInfo?id=${productId}`).then(function(response) {
+        return response.json();
+      }).then(function(data) {
+        productDiv = template.content.cloneNode(true)
+
+        productDiv.querySelector(".name").textContent = data.name
+        productDiv.querySelector(".price").textContent = `£${data.price}`
+        productDiv.querySelector(".description").textContent = data.description
+        productDiv.querySelector(".removeProduct").id = data.id
+        productDiv.querySelector(".removeProduct").addEventListener("click", () => {
+            let cart = localStorage.getItem("cart")
+            for (let i = 0; i < cart.length; i++) {
+                if (cart[i] === data.id) {
+                    cart.splice(i, 1)
+                    break
+                }
+            }
+            localStorage.setItem("cart", cart)
+        })
+
+      }).catch(function() {
+        console.log("Error Occured!");
+      });
+}
 
 if (cart) {
-
+    cart.forEach(productId => {
+        addItem(productId)
+    })
 } else {
     
 }
