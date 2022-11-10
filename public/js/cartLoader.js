8cc337f1442f5eb64464d075acb81a4ac797f928
Bartlomiej Dlugosz <bartlomiejd15@gmail.com>
2022-11-10 18:08:43 +0000

    Fixed bugs in the cart system

--- public/js/cartLoader.js
+++ public/js/cartLoader.js
@@ -1,39 +1,44 @@
-let cart = localStorage.getItem("cart")
-const container = document.querySelector(".container")
+let cart = JSON.parse(localStorage.getItem("cart"))
+const container = document.querySelector(".row")
 const template = document.querySelector("#template")
 
 async function addItem(productId) {
-    fetch(`/getProductInfo?id=${productId}`).then(function(response) {
+    fetch(`/getProductInfo?id=${productId}`).then(function (response) {
         return response.json();
-      }).then(function(data) {
+    }).then(function (data) {
+        console.log(data)
         productDiv = template.content.cloneNode(true)
 
         productDiv.querySelector(".name").textContent = data.name
         productDiv.querySelector(".price").textContent = `£${data.price}`
         productDiv.querySelector(".description").textContent = data.description
-        productDiv.querySelector(".removeProduct").id = data.id
+        productDiv.querySelector(".removeProduct").id = data._id
         productDiv.querySelector(".removeProduct").addEventListener("click", () => {
-            let cart = localStorage.getItem("cart")
+            cart = JSON.parse(localStorage.getItem("cart"))
             for (let i = 0; i < cart.length; i++) {
-                if (cart[i] === data.id) {
+                console.log(`${cart[i]} ${data._id}`)
+                if (cart[i] === data._id) {
                     cart.splice(i, 1)
                     break
                 }
             }
-            localStorage.setItem("cart", cart)
+            localStorage.setItem("cart", JSON.stringify(cart))
         })
 
         container.appendChild(productDiv)
 
-      }).catch(function() {
+    }).catch(function () {
         console.log("Error Occured!");
-      });
+    });
 }
-
-if (cart) {
+console.log(cart)
+if (cart && cart === []) {
     cart.forEach(productId => {
         addItem(productId)
     })
 } else {
-    
+    let info = document.createElement("p")
+    info.classList.add("lead")
+    info.textContent = "Sorry, but you don't have any products in your cart"
+    container.appendChild(info)
 }
