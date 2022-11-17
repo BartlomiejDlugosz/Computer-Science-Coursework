971fb2f3358a309fa650ec789806d399a2196e46
Bartlomiej Dlugosz <94141158+BartyCoding@users.noreply.github.com>
2022-11-17 15:06:12 +0000

    Improved minor bug

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.post("/createOrder", async (req, res) => {
 
         res.send({ status: "Success" })
     } else {
-        res.send({status: "Out of stock", errorMessage: `The product "${valid.product.name}" is out of stock, and has been removed from the cart for you.`, product})
+        res.send({status: "Out of stock", errorMessage: `The product "${valid.product.name}" is out of stock, and has been removed from the cart for you.`, product: valid.product})
     }
 })
 
