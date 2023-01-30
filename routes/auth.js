2ca7a0f1b3fc5b6c14f754223ce5a458f4550074
Bartlomiej Dlugosz <94141158+BartyCoding@users.noreply.github.com>
2023-01-30 10:57:05 +0000

    Testing

--- routes/auth.js
+++ routes/auth.js
@@ -106,7 +106,7 @@ router.get("/order", isLoggedIn, catchAsync(async (req, res) => {
     try {
         await cart.validateCart()
     } catch(e) {
-        req.flash("error", "One or more items in your cart are out of stock and have been removed for you")
+        req.flash("error", `One or more items in your cart are out of stock and have been removed for you ${e.msg}`)
         return res.redirect("/cart")
     }
 
