5061d96b73cdf26232eddb5aaab9035e6db19be3
Bartlomiej Dlugosz <94141158+BartyCoding@users.noreply.github.com>
2023-02-02 15:31:19 +0000

    Simplified delete category middleware

--- Models/Category.js
+++ Models/Category.js
@@ -25,18 +25,7 @@ const categorySchema = new mongoose.Schema({
 
 // Defines the middleware for deleting a category. This removes the category from all the products containing the category
 categorySchema.pre("findOneAndDelete", async function (next) {
-    const products = await Product.find({categories: this.id})
-    console.log(products)
-    console.log(this.id)
-    for (let product of products) {
-        for (let i = 0; i < product.categories.length; i++) {
-            if (product.categories[i].toString() === this.id.toString()) {
-                product.categories.splice(i, 1)
-                await product.save()
-                break
-            }
-        }
-    }
+    const products = await Product.updateMany({categories: this.id}, {$pull: {categories: this.id}})
     next()
 })
 
